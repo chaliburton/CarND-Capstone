850034f9b30125eae01e7dea5fbb322b7201495c
SorenRusbjerg <rusbjerg.consulting@gmail.com>
2020-02-11 20:51:09 +0100

    Switch for Sim/Carla inserted in classifier
    Inserted test of correct image shape before classification

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -8,43 +8,49 @@ from PIL import Image
 import cv2
 
 class TLClassifier(object):
-    def __init__(self):
-         ## Load model weights
-         weight_path = 'light_classification/ssd7_TL_epoch-03_weights_simulator.h5'
+    def __init__(self,is_site):
+        ## Load model weights site or simulator
+        if is_site:
+            weight_path = 'light_classification/ssd7_TL_epoch-03_weights_Carla.h5'
+        else:
+            weight_path = 'light_classification/ssd7_TL_epoch-03_weights_simulator.h5'
+        
+        self.img_height = 600 # Height of the input images
+        self.img_width = 800 # Width of the input images
+        self.img_channels = 3 # Number of color channels of the input images
+        intensity_mean = 127.5 # Set this to your preference (maybe `None`). The current settings transform the input pixel values to the interval `[-1,1]`.
+        intensity_range = 127.5 # Set this to your preference (maybe `None`). The current settings transform the input pixel values to the interval `[-1,1]`.
+        n_classes = 8 # Number of positive classes
+        scales = [0.08, 0.16, 0.32, 0.64, 0.96] # An explicit list of anchor box scaling factors. If this is passed, it will override `min_scale` and `max_scale`.
+        aspect_ratios = [0.5, 1.0, 2.0] # The list of aspect ratios for the anchor boxes
+        two_boxes_for_ar1 = True # Whether or not you want to generate two anchor boxes for aspect ratio 1
+        steps = None # In case you'd like to set the step sizes for the anchor box grids manually; not recommended
+        offsets = None # In case you'd like to set the offsets for the anchor box grids manually; not recommended
+        clip_boxes = False # Whether or not to clip the anchor boxes to lie entirely within the image boundaries
+        variances = [1.0, 1.0, 1.0, 1.0] # The list of variances by which the encoded target coordinates are scaled
+        normalize_coords = True # Whether or not the model is supposed to use coordinates relative to the image size
+        self.model = build_model(image_size=(self.img_height, self.img_width, self.img_channels),
+                            n_classes=n_classes,
+                            mode='inference',
+                            l2_regularization=0.0005,
+                            scales=scales,
+                            aspect_ratios_global=aspect_ratios,
+                            aspect_ratios_per_layer=None,
+                            two_boxes_for_ar1=two_boxes_for_ar1,
+                            steps=steps,
+                            offsets=offsets,
+                            clip_boxes=clip_boxes,
+                            variances=variances,
+                            normalize_coords=normalize_coords,
+                            subtract_mean=intensity_mean,
+                            divide_by_stddev=intensity_range)
 
-         img_height = 600 # Height of the input images
-         img_width = 800 # Width of the input images
-         img_channels = 3 # Number of color channels of the input images
-         intensity_mean = 127.5 # Set this to your preference (maybe `None`). The current settings transform the input pixel values to the interval `[-1,1]`.
-         intensity_range = 127.5 # Set this to your preference (maybe `None`). The current settings transform the input pixel values to the interval `[-1,1]`.
-         n_classes = 8 # Number of positive classes
-         scales = [0.08, 0.16, 0.32, 0.64, 0.96] # An explicit list of anchor box scaling factors. If this is passed, it will override `min_scale` and `max_scale`.
-         aspect_ratios = [0.5, 1.0, 2.0] # The list of aspect ratios for the anchor boxes
-         two_boxes_for_ar1 = True # Whether or not you want to generate two anchor boxes for aspect ratio 1
-         steps = None # In case you'd like to set the step sizes for the anchor box grids manually; not recommended
-         offsets = None # In case you'd like to set the offsets for the anchor box grids manually; not recommended
-         clip_boxes = False # Whether or not to clip the anchor boxes to lie entirely within the image boundaries
-         variances = [1.0, 1.0, 1.0, 1.0] # The list of variances by which the encoded target coordinates are scaled
-         normalize_coords = True # Whether or not the model is supposed to use coordinates relative to the image size
-         self.model = build_model(image_size=(img_height, img_width, img_channels),
-                             n_classes=n_classes,
-                             mode='inference',
-                             l2_regularization=0.0005,
-                             scales=scales,
-                             aspect_ratios_global=aspect_ratios,
-                             aspect_ratios_per_layer=None,
-                             two_boxes_for_ar1=two_boxes_for_ar1,
-                             steps=steps,
-                             offsets=offsets,
-                             clip_boxes=clip_boxes,
-                             variances=variances,
-                             normalize_coords=normalize_coords,
-                             subtract_mean=intensity_mean,
-                             divide_by_stddev=intensity_range)
 
-         # 2: Load some weights
-         self.model.load_weights(weight_path, by_name=True)
-         np.set_printoptions(precision=2, suppress=True, linewidth=90)
+        # Load the model weights        
+        self.model.load_weights(weight_path, by_name=True)
+
+        # Print numpy arrays in nice format when using print
+        np.set_printoptions(precision=2, suppress=True, linewidth=90)
         
 
     def get_classification(self, image):
@@ -63,8 +69,13 @@ class TLClassifier(object):
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         # add a fourth batch dimension to array
         image = np.expand_dims(image, axis=0)
+
         ## Predict images class
-        y_pred = self.model.predict(image)
+        if image.shape==(1, self.img_height, self.img_width, self.img_channels):
+            y_pred = self.model.predict(image)
+        else:
+            rospy.logwarn("tl_classifier: Wrong image shape: ", image.shape)
+            return TrafficLight.UNKNOWN
 
         # Filter predictions
         confidence_threshold = 0.7
